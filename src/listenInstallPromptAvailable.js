ba5f470be1232d5ce3611e9888d978b8d88c2d0d
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-03 09:36:19 +0100

    update listenInstallPromptAvailable

--- src/listenInstallPromptAvailable.js
+++ src/listenInstallPromptAvailable.js
@@ -1,19 +1,56 @@
+import { listenAppInstalled } from "./listenAppInstalled.js"
+
 export const listenInstallPromptAvailable = (callback) => {
-  window.addEventListener("beforeinstallprompt", (beforeinstallpromptEvent) => {
-    const prompt = async () => {
-      beforeinstallpromptEvent.prompt()
-      const choiceResult = await beforeinstallpromptEvent.userChoice
-      if (choiceResult.outcome === "accepted") {
-        beforeinstallpromptEvent = undefined
-        return true
-      }
-      return false
+  const installPromptListener = () => {
+    const { beforeinstallpromptEvent } = window
+    if (beforeinstallpromptEvent) {
+      const prompt = beforeInstallPromptEventToPrompt(beforeinstallpromptEvent, () => {
+        window.beforeinstallpromptEvent = undefined
+      })
+      callback(prompt)
+      return () => {}
     }
-    callback({ prompt })
+
+    const onbeforeinstallprompt = (beforeinstallpromptEvent) => {
+      beforeinstallpromptEvent.preventDefault()
+      const prompt = beforeInstallPromptEventToPrompt(beforeinstallpromptEvent)
+      callback({ prompt })
+    }
+
+    window.addEventListener("beforeinstallprompt", onbeforeinstallprompt)
+    return () => {
+      window.removeEventListener("beforeinstallprompt", onbeforeinstallprompt)
+    }
+  }
+
+  const removePromptListener = installPromptListener()
+
+  const removeAppInstalledListener = listenAppInstalled(() => {
+    // prompt "becomes" unavailable if user installs app
+    // it can happen if user installs app manually from browser toolbar
+    // in that case there is no point showing the install
+    // button in the ui
+    callback(null)
+    removePromptListener()
   })
+
+  return () => {
+    removePromptListener()
+    removeAppInstalledListener()
+  }
 }
 
-// listenInstallPromptAvailable(({ prompt }) => {
+const beforeInstallPromptEventToPrompt = async (beforeinstallpromptEvent, onaccept = () => {}) => {
+  beforeinstallpromptEvent.prompt()
+  const choiceResult = await beforeinstallpromptEvent.userChoice
+  if (choiceResult.outcome === "accepted") {
+    onaccept()
+    return true
+  }
+  return false
+}
+
+// listenInstallPromptAvailable((prompt) => {
 //   document.querySelector("#install").disabled = false
 //   document.querySelector("#install").onclick = async () => {
 //     const accepted = await prompt()
